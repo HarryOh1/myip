2ebd6684951534a5e111db0669fbcc1530bd9303
HarryOh1 <ohyh99@naver.com>
2023-06-12 13:32:21 +0900

    02

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3030;
 
-// // Define a route to handle the root URL ("/")
-// app.get("/", (req, res) => {
-//   const ipAddress = req.ip;
-//   res.send(`Your IP address is: ${ipAddress}`);
-// });
-
+// Define a route to handle the root URL ("/")
 app.get("/", (req, res) => {
-  const ipAddress =
-    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  const ipAddress = req.ip;
   res.send(`Your IP address is: ${ipAddress}`);
 });
 
+// app.get("/", (req, res) => {
+//   const ipAddress =
+//     req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+//   res.send(`Your IP address is: ${ipAddress}`);
+// });
+
 // Start the server
-const port = 3000; // Choose the desired port number
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
