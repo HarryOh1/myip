2b7705020f40e13781a174dd04269718fa6de672
HarryOh1 <ohyh99@naver.com>
2023-06-12 13:55:10 +0900

    03

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-// Define a route to handle the root URL ("/")
-app.get("/", (req, res) => {
-  const ipAddress = req.ip;
-  res.send(`Your IP address is: ${ipAddress}`);
-});
-
+// // Define a route to handle the root URL ("/")
 // app.get("/", (req, res) => {
-//   const ipAddress =
-//     req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+//   const ipAddress = req.ip;
 //   res.send(`Your IP address is: ${ipAddress}`);
 // });
+
+app.get("/", (req, res) => {
+  const ipAddress =
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  res.send(`Your IP address is: ${ipAddress}`);
+});
 
 // Start the server
 app.listen(PORT, () => {
